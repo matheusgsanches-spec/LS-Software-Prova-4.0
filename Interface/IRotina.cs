d51df195b132f1c4cf2a503d229f7042e412f1a0
Matheus Sanches <matheus.g.sanches@aluno.senai.br>
2026-05-31 02:14:47 -0300

    first commit

---
+++ Interface/IRotina.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prova_lulu___Sanxis.Interface;
+
+public interface IRotina
+{
+    void Executar();
+    string ObterDescricao();
+}
